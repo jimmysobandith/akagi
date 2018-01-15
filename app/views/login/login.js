1c01ff60a58fa82f86f39106d920fcc584cb933d
Jimmy <jimmy.sobandith@outlook.fr>
2018-01-15 17:01:13 +0100

    Design + connexion

--- app/views/login/login.js
+++ app/views/login/login.js
@@ -1,6 +1,8 @@
 var dialogsModule = require("ui/dialogs");
 var observableModule = require("data/observable")
 var ObservableArray = require("data/observable-array").ObservableArray;
+var searchBarModule = require("tns-core-modules/ui/search-bar");
+var searchBar = new searchBarModule.SearchBar();
 var page;
 
 var GroceryListViewModel = require("../../shared/view-models/grocery-list-view-model");
